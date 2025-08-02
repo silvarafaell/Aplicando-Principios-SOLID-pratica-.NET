c7ff50d0ee1c27e709608a62ba5987ed445d0ab2
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-08-02 11:26:58 -0300

    Encapsulamento e modelo de dominio - pratica

---
+++ Fundamentos da programação Orientada a Objetos/ModeloDominio/CursoFoop_ModeloDominio/ClienteAnemico.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoFoop_ModeloDominio
+{
+    class ClienteAnemico
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+    }
+}
---
+++ Fundamentos da programação Orientada a Objetos/ModeloDominio/CursoFoop_ModeloDominio/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CursoFoop_ModeloDominio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClienteRico cliente = new ClienteRico(-1,"", null);
+               
+            Console.WriteLine($"{cliente.Id} {cliente.Nome} {cliente.Endereco}");
+            Console.ReadLine();
+        }
+
+        private static ClienteAnemico ClienteAnemicoTeste()
+        {
+            ClienteAnemico cliente = new ClienteAnemico();
+            cliente.Id = -1;
+            cliente.Nome = "";
+            cliente.Endereco = null;
+            return cliente;
+        }
+    }
+}
