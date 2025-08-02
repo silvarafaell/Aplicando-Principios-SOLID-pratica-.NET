eb64653db5c829b00c2e4592c1ff82d639d78dbb
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-08-02 09:56:50 -0300

    A palavra this

---
+++ Fundamentos da programação Orientada a Objetos/PalavraThis/CursoFoop_Fundamentos_This/Funcionario.cs
@@ -0,0 +1,17 @@
+﻿namespace CursoFoop_Fundamentos_This
+{
+    class Funcionario
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Funcionario(string Nome, string Email)
+        {
+            // Use this para qualificar as propriedades
+            // Nome e Email evitando confusão com os nomes
+            // dos parâmetros usados no construtor
+            this.Nome = Nome;
+            this.Email = Email;
+        }
+    }
+}
---
+++ Fundamentos da programação Orientada a Objetos/PalavraThis/CursoFoop_Fundamentos_This/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CursoFoop_Fundamentos_This
+{
+    class Teste
+    {
+        public string Nome { get; set; }
+        public Teste(Exemplo exemplo)
+        {
+            // Usa o nome da instância Perl
+            this.Nome = exemplo.Nome;
+            Console.WriteLine($"Teste : {this.Nome}");
+        }
+    }
+
+    class Exemplo
+    {
+        public string Nome { get; set; }
+        public Exemplo(string nome)
+        {
+            this.Nome = nome;
+            // Passa a instância this como um parâmetro
+            Teste teste = new Teste(this);
+            // A instância Teste agora possui o mesmo nome
+            Console.WriteLine($"Exemplo : {teste.Nome}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exemplo exemplo = new Exemplo("This");
+            Console.ReadLine();
+        }
+    }
+}
