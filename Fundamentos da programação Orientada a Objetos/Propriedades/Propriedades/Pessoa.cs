cb0558c005a9b0703dee3b3d3b0e26d75d6a4fe8
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-08-02 11:01:45 -0300

    propriedades e encapsulamento

---
+++ Fundamentos da programação Orientada a Objetos/Propriedades/Propriedades/Pessoa.cs
@@ -0,0 +1,40 @@
+﻿namespace Aula_Propriedades
+{
+    public class Pessoa
+    {
+        private string nome;
+        private string genero;
+        private int idade;
+
+        public Pessoa(string nome)
+        {
+            this.Nome = nome;
+        }
+        public string Nome { get;  }
+
+        public string Genero
+        {
+            set
+            {
+                genero = value;
+                System.Console.WriteLine(genero);
+            }
+        }
+
+        public int Idade
+        {
+            set
+            {
+                if(value < 18)
+                {
+                    System.Console.WriteLine("Idade tem que ser maior que 18");
+                }
+                else
+                {
+                    this.idade = value;
+                }
+            }
+            get { return idade; }
+        }
+    }
+}
---
+++ Fundamentos da programação Orientada a Objetos/Propriedades/Propriedades/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Aula_Propriedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa("Maria");
+            p1.Genero = "masculino";
+            p1.Idade = 19;
+
+            Console.WriteLine(p1.Nome);
+            Console.WriteLine(p1.Idade);
+        }
+    }
+}
