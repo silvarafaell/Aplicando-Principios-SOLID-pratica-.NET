6cf7f5bb87c39cb637c3a0b12d50382eca2fe370
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-08-02 23:32:58 -0300

    metodos de extensao

---
+++ Fundamentos da programação Orientada a Objetos/MethodosExtensao/CursoFoop_MethodosExtensao/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CursoFoop_MethodosExtensao.Utils;
+
+namespace CursoFoop_MethodosExtensao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var texto1 = "curso";
+            var texto2 = "madalena";
+
+            //método de extensão
+            texto1 = texto1.CaixaAltaPrimeiraLetra();
+            texto2 = texto2.CaixaAltaPrimeiraLetra();
+            
+            Console.WriteLine(texto1);
+            Console.WriteLine(texto2);
+
+            Console.ReadLine();
+        }
+    }
+}
