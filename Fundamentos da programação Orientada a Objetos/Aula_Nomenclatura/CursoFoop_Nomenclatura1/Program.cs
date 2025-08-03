224f1b3a562a26c1f96880b86ae8628ec70c9997
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-08-03 10:50:28 -0300

    Nomenclatura

---
+++ Fundamentos da programação Orientada a Objetos/Aula_Nomenclatura/CursoFoop_Nomenclatura1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace CursoFoop_Nomenclatura1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //válidos
+            //CamelCasing
+            string nomeAluno = "joao";
+            int idadeMinima = 10;
+            int IdadeMinima = 20;
+            double _taxa = 1.25;
+            int numero_aluno1 = 10;
+            double y2x5_w7 = 3.4;
+            var \u0043sharp = "Csharp";
+
+            //Inválidos
+            bool verifica#conta = true;
+                int decimal = 34.00;
+            string 1sexo = "masculino";
+            string #cliente ="teste";
+            int idade@Maxima = 99;
+
+            var nome = "Maria";
+            var valor = 10;
+            
+        }
+    }
+
+    //PascalCasing
+    class CalculaDesconto
+    {
+        //CamelCasing
+        double taxaEncargos = 1.99;
+        public int ValorImpostoTotal { get; set; }
+
+        //PascalCasing
+        public double CalculoEncargos(double valorTotal)
+        {
+            return valorTotal * taxaEncargos;
+        }
+    }
+}
