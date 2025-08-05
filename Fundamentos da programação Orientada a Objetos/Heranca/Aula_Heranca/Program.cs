d96fb63e7cf73a1f5a5d97df51949f3d56e17214
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-08-04 22:17:26 -0300

    Heranca - conceitos basicos

---
+++ Fundamentos da programação Orientada a Objetos/Heranca/Aula_Heranca/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Aula_Heranca
+{
+    class Conta
+    {
+        public int Numero { get; set; }
+        public double Saldo { get; set; }
+
+        public Conta()
+        {
+            Console.WriteLine("Construtor da classe Conta");
+        }
+
+        protected void GetSaldo()
+        {
+            Console.WriteLine("Saldo 99,99");
+        }
+    }
+    class ContaPoupanca : Conta
+    {
+        public int JurosMensais { get; set; }
+
+        protected new void GetSaldo()
+        {
+            Console.WriteLine("Saldo 77,77");
+        }
+
+        static void Main(string[] args)
+        {
+            ContaPoupanca cp = new ContaPoupanca();
+            cp.Numero = 100;
+            cp.Saldo = 99.99;
+            cp.GetSaldo();
+        }
+    }
+}
