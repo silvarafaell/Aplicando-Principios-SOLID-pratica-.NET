45bfa92ec4af279e519e8ecf3545a8e82f8422a1
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-08-16 23:14:06 -0300

    diagrama de classes

---
+++ UML, Herança, Interface e Polimorfismo/DiagramaClasses/CursoFoop_DiagramaClasses/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoFoop_DiagramaClasses
+{
+    public class Pessoa
+    {
+        public Pessoa() { }
+        public Pessoa(string nome, int idade, string sexo)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+            this.Sexo = sexo;
+        }
+
+        private DateTime dataNascimento = new DateTime(1980, 9, 11);
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Sexo { get; set; }
+
+        public void IdentificarPessoa()
+        {
+            Console.WriteLine($"{this.Nome} - {this.Idade} - {this.Sexo}");
+        }
+    }
+}
---
+++ UML, Herança, Interface e Polimorfismo/DiagramaClasses/CursoFoop_DiagramaClasses/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CursoFoop_DiagramaClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Diagrama de Classes");
+        }
+    }
+}
