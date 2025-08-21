58330c8e161804a07ad088d8a4c373f49405a4aa
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-08-20 23:38:52 -0300

    UML - agregacao

---
+++ UML, Herança, Interface e Polimorfismo/Agregacao/Composicao_Agregacao/Departamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Agregacao
+{
+    class Departamento : IDisposable
+    {
+        public string Nome { get; set; }
+        public List<Professor> Professores { get; set; }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ UML, Herança, Interface e Polimorfismo/Agregacao/Composicao_Agregacao/Professor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agregacao
+{
+    class Professor : IDisposable
+    {
+        public string Nome { get; set; }
+        public void Dispose() { }
+    }
+}
