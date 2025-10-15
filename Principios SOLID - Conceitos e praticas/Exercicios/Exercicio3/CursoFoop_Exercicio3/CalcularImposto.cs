e9c4e7736ec4ebd2cae2298ffa95f216a77d9416
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-10-14 23:05:45 -0300

    exercicios3

---
+++ Principios SOLID - Conceitos e praticas/Exercicios/Exercicio3/CursoFoop_Exercicio3/CalcularImposto.cs
@@ -0,0 +1,24 @@
+﻿namespace CursoFoop_Exercicio3
+{
+    class CalcularImposto
+    {
+        public static decimal Calcular(decimal valor, decimal deducao, string pais)
+        {
+            decimal valorImposto = 0;
+            decimal valorBase = valor - deducao;
+            switch (pais)
+            {
+                case "Brazil":
+                    //código cálculo
+                    break;
+                case "USA":
+                    //código cálculo
+                    break;
+                case "UK":
+                    //código cálculo
+                    break;
+            }
+            return valorImposto;
+        }
+    }
+}
